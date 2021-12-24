4b13c131a4d0ec4c7a97b4c4fdb0fde96731d2d2
Kurumi-007 <fly07sal@gmail.com>
2021-12-23 21:42:57 -0600

    crud part2

--- app.js
+++ app.js
@@ -4,16 +4,33 @@ firebase.initializeApp({
     authDomain: "prueba-bf929.firebaseapp.com",
     projectId: "prueba-bf929"
   });
-  
+  //Inicializando 
   var db = firebase.firestore();
-  db.collection("users").add({
-    first: "Deya",
-    last: "Programming",
-    born: 2021
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
+
+  //Agregar documentos
+
+  //.add agregar un id de manera automatica
+
+function guardar(){
+    
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var fecha = document.getElementById('fecha').value;
+
+    db.collection("users").add({
+        first: nombre,
+        last: apellido,
+        born: fecha
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+        document.getElementById('nombre').value = '';
+        document.getElementById('apellido').value = '';
+        document.getElementById('fecha').value = '';
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+
+}
+  
